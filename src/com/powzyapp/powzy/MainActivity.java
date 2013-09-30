9282b845859641cbd78d5d58b824c889d0449b06
Ashish  Bindal <ashish.bindal@epfl.ch>
2013-09-30 11:28:45 +0200

    IMPR: home Screen- added missions

--- src/com/powzyapp/powzy/MainActivity.java
+++ src/com/powzyapp/powzy/MainActivity.java
@@ -7,18 +7,26 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
 public class MainActivity extends BaseActivity {
-
+	
+	
+	
+	//UI
+	ImageButton discover;
+	ImageButton taskBtn;
+	ImageButton feedBtn;
+	
+	FrameLayout frameLayout;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		
-		ImageButton discover = (ImageButton) findViewById(R.id.first_discover);       
-		
+		setupUI();
 		discover.setOnClickListener(new OnClickListener() {			
 			@Override
 			public void onClick(View v) {
@@ -27,9 +35,18 @@ public class MainActivity extends BaseActivity {
 			}
 		});
 		
-		
+		//discover.setOnClickListener(new OnClickListner() {)
+		//frameLayout.get
 	}
-
+	
+	public void setupUI() {
+		discover = (ImageButton) findViewById(R.id.first_discover);      
+		taskBtn = (ImageButton) findViewById(R.id.task_btn);
+		feedBtn = (ImageButton) findViewById(R.id.feed_btn);
+		
+		frameLayout = (FrameLayout) findViewById(R.id.bottom_framelayout);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
